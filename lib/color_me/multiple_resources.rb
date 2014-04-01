20007d9b4ef86b5723a01b3efcda47d503d29a5f
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-04-02 01:16:50 +0900

    Rename ColorMe::Bogus::get_all to ColorMe::Bogus::get. Old ColorMe::Bogus::get is named ColorMe::Bogus::partial_get. Bunm the version to 0.0.5. #16

--- lib/color_me/multiple_resources.rb
+++ lib/color_me/multiple_resources.rb
@@ -5,23 +5,22 @@ module ColorMe
     end
 
     def get(params={})
-      url = endpoint + ColorMe.build_query(params)
-      res = ColorMe.api.get(url)
-      ColorMe.parse_json(res.body)
-    end
-
-    def get_all
-      res = get(limit: 1)
-      total = res[:meta][:total]
+      res = partial_get(params)
+      total = [res[:meta][:total], params[:limit]].reject(&:nil?).min
 
       while res[collection_key].size < total do
         limit = [max_limit, total - res[collection_key].size].min
         offset = res[collection_key].size
-        new_res = get(limit: limit, offset: offset)
+        new_res = partial_get(params.merge(limit: limit, offset: offset))
         res[collection_key] += new_res[collection_key]
       end
-
       res
+    end
+
+    def partial_get(params={})
+      url = endpoint + ColorMe.build_query(params)
+      res = ColorMe.api.get(url)
+      ColorMe.parse_json(res.body)
     end
   end
 end
--- spec/color_me/sales_spec.rb
+++ spec/color_me/sales_spec.rb
@@ -1,41 +1,47 @@
 require 'spec_helper'
 
-describe ColorMe::Sales do
-  describe ".get" do
-    let(:res){ ColorMe::Sales.get }
+describe ColorMe::Sales, online: true do
+  describe "::get" do
+    context "with no arguments" do
+      before(:all){ @res = ColorMe::Sales.get }
 
-    it "should be Hash", online: true do
-      expect(res).to be_an_instance_of(Hash)
-    end
+      it "should be Hash" do
+        expect(@res).to be_an_instance_of(Hash)
+      end
 
-    it "should have a key ':sales'", online: true do
-      expect(res[:sales]).not_to be_nil
-    end
+      it "should have a key ':sales'" do
+        expect(@res[:sales]).not_to be_nil
+      end
 
-    describe "limit" do
-      let(:res){ ColorMe::Sales.get(limit: 1) }
+      it "should have all sales" do
+        expect(@res[:sales].size).to eq(@res[:meta][:total])
+      end
 
-      it "should be able to limit sales", online: true do
-        expect(res[:sales].size).to eq(1)
+      it "should have unique sales" do
+        sale_ids = @res[:sales].map{|p| p[:id] }
+        expect(sale_ids.uniq.size).to eq(@res[:meta][:total])
       end
     end
-  end
 
-  describe ".get_all" do
-    let(:res){ ColorMe::Sales.get_all }
+    context "with arguments" do
+      describe ":limit" do
+        before(:all){ @res = ColorMe::Sales.get(limit: 1) }
 
-    it "should be Hash", online: true do
-      p res[:sales].size
-      expect(res).to be_an_instance_of(Hash)
-    end
+        it "should be able to limit sales" do
+          expect(@res[:sales].size).to eq(1)
+        end
+      end
 
-    it "should have all sales", online: true do
-      expect(res[:sales].size).to eq(res[:meta][:total])
-    end
+      describe ":after" do
+        before :all do
+          @time = Time.now - 3600 * 48
+          @res = ColorMe::Sales.get(after: @time.strftime("%Y-%m-%d"))
+        end
 
-    it "should have unique sales", online: true do
-      sale_ids = res[:sales].map{|p| p[:id] }
-      expect(sale_ids.uniq.size).to eq(res[:meta][:total])
+        it "should be able to get recent sales" do
+          expect(@res[:sales].all?{|s| s[:make_date] >= @time.to_i}).to be_true
+        end
+      end
     end
   end
 end
